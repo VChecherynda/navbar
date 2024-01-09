d59b35ea73e5e3d84349c4c2c76939562c6ea0cc
vchecherynda <vchecherynda@shutterstock.com>
2024-01-09 02:22:42 +0200

    Add fixes and port

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,7 +1,8 @@
-export default function Root(props) {
+export default function Root() {
   return (
     <section>
       <h1>Navbar 123</h1>
+      {/* <Button type="primary">Primary Button</Button> */}
     </section>
   );
 }

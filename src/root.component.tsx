e310a4f90ca97cb091b4b7ca665a9a349d02f2cd
vadym <checherinda.vadym@gmail.com>
2024-01-10 17:15:11 +0200

    Add esmodule and commonjs syntax

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,8 +1,18 @@
+// Here problem with esmodules. I receive undefined from 
+// utils api. But if we use commonjs syntax all work.
+// Uncomment and check
+
+// const api = require("@home/api");
+
+import api from "@home/api";
+
 export default function Root() {
+
+  console.log('[api]', api);
+
   return (
     <section>
       <h1>Navbar 123</h1>
-      {/* <Button type="primary">Primary Button</Button> */}
     </section>
   );
 }

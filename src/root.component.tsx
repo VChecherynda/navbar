9d7fc09aa79c6bc559e1a08ffdb651cfb92555ed
vadym <checherinda.vadym@gmail.com>
2024-01-15 12:48:21 +0200

    Refactor and add login logic

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,18 +1,64 @@
-// Here problem with esmodules. I receive undefined from 
-// utils api. But if we use commonjs syntax all work.
-// Uncomment and check
+import { useEffect, useState } from "react";
+const { Avatar, UserOutlined, Typography, Button } = require("@home/components");
 
-// const api = require("@home/api");
+const api = require('@home/api');
 
-import api from "@home/api";
+const styles = {
+  navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingRight: 24,
+    paddingLeft: 24,
+  },
+  user: {
+    display: 'flex'
+  },
+  userAva: {
+    marginRight: '12px'
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '12px'
+  }
+}
 
-export default function Root() {
+export default function Root(props) { 
+  const [userData, setUserData] = useState(undefined);
 
-  console.log('[api]', api);
+  const handleLogin = async () => {
+    const loginUserData = await api.login();
+    setUserData(loginUserData)
+  }
+
+  const handleLogout = () => {
+    const loginUserData = api.logout();
+    setUserData(loginUserData)
+  }
+
+  useEffect(() => {
+    const loginUserData = api.getLoginUser();
+
+    if (loginUserData) {
+      setUserData(loginUserData)
+    }
+  }, [])
 
   return (
-    <section>
-      <h1>Navbar 123</h1>
+    <section style={styles.navbar}>
+      <p>Logo</p>
+
+      <div>
+        {userData ? 
+          <div style={styles.user}>
+            <Avatar style={styles.userAva} size={32} icon={<UserOutlined />} />
+            <Typography style={styles.userName}>{userData.name}</Typography>
+            <Button type="primary" onClick={handleLogout}>Logout</Button>
+          </div>:
+          <Button type="primary" onClick={handleLogin}>Login</Button>
+        }
+      </div>
     </section>
   );
 }
